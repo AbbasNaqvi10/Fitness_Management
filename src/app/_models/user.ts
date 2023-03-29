8e554fd46459f8a9a0cb5149895cebbc5a6e1c93
AbbasNaqvi10 <syedabbasnaqvi10@gmail.com>
2023-03-30 03:41:01 +0500

    initial comment

---
+++ src/app/_models/user.ts
@@ -0,0 +1,13 @@
+﻿export class User {
+    id?: string;
+    fullname?: string;
+    dateOfBirth?: string;
+    gender?: string;
+    membershipType?: string;
+    membershipStartDay?: string;
+    contact?: string;
+    email?: string;
+    residentialAddress?: string;
+    emergencyContact?: string;
+    medical?: string;
+}
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { RegisterComponent } from './account/register.component';
+// import { AuthGuard } from './_helpers';
+
+const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
+const usersModule = () => import('./users/users.module').then(x => x.UsersModule);
+
+const routes: Routes = [
+    { path: '', component:RegisterComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'users', loadChildren: usersModule, },
+
+    { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
